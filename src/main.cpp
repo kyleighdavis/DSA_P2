79fbd23daaf36547836c64348931b7729b29e75a
Ariya0829 <mouthapongariya@gmail.com>
2025-10-27 20:02:56 -0400

    commitpull

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,7 @@ using namespace bridges;
 
 int main() {
     try {
-        // ✅ note the 0 as first argument (assignment number)
         Bridges bridges(0, "kyleighdavis", "753555741873");
-
         bridges.setTitle("A Simple Adjacency list based Graph Example");
         bridges.setDescription("Demonstrate how to create a graph with a few nodes and display it");
 
