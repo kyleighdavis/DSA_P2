41e78afcfcdf740aaf8fc31487a916a7e09b37b9
megawall212 <lzy4656@gmail.com>
2025-10-29 14:04:20 -0400

    add algorithm part

--- src/example.cpp
+++ src/example.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <cmath>
+#include <map>
 using namespace std;
 
 #include "Bridges.h"
@@ -9,6 +11,21 @@ using namespace std;
 
 
 using namespace bridges;
+
+double getDistance(double lat1, double lon1, double lat2, double lon2){
+	double rad = M_PI / 180.0;
+	double latitude_distance = (lat2 - lat1) * rad;
+	double longitude_distance = (lon2 - lon1) * rad;
+
+	lat1 = lat1 * rad;
+	lat2 = lat2 * rad;
+
+	double half_chord = pow(sin(latitude_distance / 2), 2) + pow(sin(longitude_distance / 2), 2) * cos(lat1) * cos(lat2);
+	double angular_distance = 2 * atan2(sqrt(half_chord), sqrt(1 - half_chord));
+
+	double earthRad = 6371.0;
+	return earthRad * angular_distance;
+}
 
 int main(int argc, char **argv) {
 
@@ -24,45 +41,89 @@ int main(int argc, char **argv) {
 	cout << "Retrieving a set of US cities" << endl;
     bridges.setDescription("A simple example of retrieving US city data from Bridges DataSource");
 
+	string state;
+	cout << "Enter state abbreviation: ";
+	getline(cin, state);
+	cout << endl;
+
 	unordered_map<string, string> city_params {
 			{"min_pop","200000"},
 			{"max_pop","1000000"},
-			{"state", "NC"},
+			{"state", state},
 			{"limit", "25"}
 		};
 
 	vector<City>  us_cities = ds.getUSCities(city_params);
 	cout << "US Cities (tested for limit of 25 cities, population over 200K, and lat/long Bounding Box: (34.025348,-85.352783), (36.800488,-75.300293):\n";
-	for (auto c : us_cities)
-		cout << "\n" << c.getCity() << "," << c.getState() << ":" <<
-			" Population: " <<  c.getPopulation()  <<
-			", Elevation: "  <<  c.getElevation()
-			<< ", Lat/Long: " << c.getLatitude() << "," << c.getLongitude();
+	for (int i = 0; i < us_cities.size(); i++){
+		cout << "\n" << us_cities[i].getCity() << "," << us_cities[i].getState() << ":" <<
+		" Population: " <<  us_cities[i].getPopulation()  <<
+		", Elevation: "  <<  us_cities[i].getElevation()
+		<< ", Lat/Long: " << us_cities[i].getLatitude() << "," << us_cities[i].getLongitude();
+	}
 
-    
-    GraphAdjList<string> city_graph;
-	string startVertex;
-	cout << "Enter us City";
-	getline(cin, startVertex);
+	bool found = false;
+	for(int i = 0; i < us_cities.size(); i++){
+		string city_state = us_cities[i].getState();
+		if(city_state == state){
+			found = true;
+		}
+	}
 
-    string fayetteville_id = "Fayetteville, NC";
-    string raleigh_id = "Raleigh, NC";
-    string charlotte_id = "Charlotte, NC";
-    city_graph.addVertex(fayetteville_id);
-    city_graph.addVertex(raleigh_id);
-    city_graph.addVertex(charlotte_id);
-	//city_graph.addVertex(input);
-    city_graph.addEdge(fayetteville_id, raleigh_id);
-    city_graph.addEdge(raleigh_id, charlotte_id);
-    city_graph.addEdge(charlotte_id, fayetteville_id);
-	//city_graph.addEdge(input, fayetteville_id);
-
-	if(city_graph.getVertex(startVertex) != nullptr){
-		city_graph.getVertex(startVertex)->getVisualizer()->setColor("red");
-		cout << "Selected City: " << startVertex << endl;
+	if(!found){
+		cout << "No cities found in " << state << endl;
+		return 0;
 	}
 	else{
-		cout << "City not found" << endl;
+		cout << "Cities found in " << state << "!" << endl;
+	}
+    
+   GraphAdjList<string, double> city_graph;
+	//format example to access edge_weights["Raleigh, NC, Charlotte, NC"];
+	map<string, double> edge_weights;
+	string startVertex;
+	string endVertex;
+	cout << endl;
+	cout << "Enter First City: ";
+	getline(cin, startVertex);
+	cout << "Enter Second City: ";
+	getline(cin, endVertex);
+	cout << endl;
+
+	for(int i = 0; i < us_cities.size(); i++){
+		City c = us_cities[i];
+		string city_id = c.getCity() + ", " + c.getState();
+		city_graph.addVertex(city_id);
+	}
+
+	for(int i = 0; i < us_cities.size(); i++){
+		for(int j = i + 1; j < us_cities.size(); j++){
+			double distance = getDistance(us_cities[i].getLatitude(), us_cities[i].getLongitude(), us_cities[j].getLatitude(), us_cities[j].getLongitude());
+
+			string city1 = us_cities[i].getCity() + ", " + us_cities[i].getState();
+			string city2 = us_cities[j].getCity() + ", " + us_cities[j].getState();
+			city_graph.addEdge(city1, city2, distance);
+			city_graph.addEdge(city2, city1, distance);
+
+			edge_weights[city1 + "," + city2] = distance;
+			edge_weights[city2 + "," + city1] = distance;
+		}
+	}
+
+
+	if(city_graph.getVertex(startVertex) == nullptr && city_graph.getVertex(endVertex) == nullptr){
+		cout << "Neither city found" << endl;
+	}
+	else if(city_graph.getVertex(startVertex) == nullptr){
+		cout << "First city not found" << endl;
+	}
+	else if(city_graph.getVertex(endVertex) == nullptr){
+		cout << "Second city not found" << endl;
+	}
+	else{
+		city_graph.getVertex(startVertex)->getVisualizer()->setColor("red");
+		city_graph.getVertex(endVertex)->getVisualizer()->setColor("red");
+		cout << "Showing fastest path from " << startVertex << " to " << endVertex << endl;
 	}
 	
     bridges.setDataStructure(&city_graph);
--- src/johnsongraph.cpp
+++ src/johnsongraph.cpp
@@ -1,40 +1,34 @@
 #include <string>
-
+#include <iostream>
+#include <cmath>
+#include <map>
 using namespace std;
 
 #include "Bridges.h"
-#include "DataSource.h"
+#include "DataSource.h" 
 #include "data_src/City.h"
 #include "GraphAdjList.h"
-#include <cstdlib>
-#include <ctime>
-#include "GraphUtils.h"
-#include <algorithm>
-#include <cctype>
-
-
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-
 
 
 using namespace bridges;
-using namespace std;
 
+double getDistance(double lat1, double lon1, double lat2, double lon2){
+	double rad = M_PI / 180.0;
+	double latitude_distance = (lat2 - lat1) * rad;
+	double longitude_distance = (lon2 - lon1) * rad;
 
-// Normalize function (convert to lowercase and remove spaces)
-string normalize(const string& s) {
-    string result;
-    for (char c : s)
-        if (!isspace(c)) result += tolower(c);
-    return result;
+	lat1 = lat1 * rad;
+	lat2 = lat2 * rad;
+
+	double half_chord = pow(sin(latitude_distance / 2), 2) + pow(sin(longitude_distance / 2), 2) * cos(lat1) * cos(lat2);
+	double angular_distance = 2 * atan2(sqrt(half_chord), sqrt(1 - half_chord));
+
+	double earthRad = 6371.0;
+	return earthRad * angular_distance;
 }
 
-// this program illustrates how to access the data of the US and World 
-//	city data
 int main(int argc, char **argv) {
-	srand(time(NULL));
+
 	// create bridges object
 	Bridges bridges (1, "AriyaM", "211314957910");
 
@@ -46,100 +40,155 @@ int main(int argc, char **argv) {
 
 	cout << "Retrieving a set of US cities" << endl;
     bridges.setDescription("A simple example of retrieving US city data from Bridges DataSource");
-	// Parameters to the query are through an unordered map
-	// you may use any subset of parameters to filter the US city data
-	// results will be filtered to satisfy all specified parameters
-	// for example if you provide population ane elevation ranges, then only those
-	// cities matching those ranges will be retrieved
 
-	// Parameters include
-	// population range - specify  min and max population values
-	// elevation range - specify  min and max elevation values - note elevation can be
-	//   				negative (below sealevel!
-	// Lat/Long bounding box -- specified by minLatLong, maxLatLong pairs of values
-	// state  - state name -- cities within that state will be retrieved
-	// city   - city name  -- if it matches, it will be retrieved
-	// limit  - limit the output to a specified number of cities
+	
+    // --- Input first state and get its cities ---
+string state1;
+cout << "Enter first state abbreviation (e.g., NC, CA, NY): ";
+getline(cin, state1);
 
+unordered_map<string, string> city_params1 {
+    {"min_pop", "0"},
+    {"max_pop", "20000000"},
+    {"state", state1},
+    {"limit", "600"}          // fetch small number for BRIDGES
+};
 
-	// return upto 10 cities in the North Carolina, using the 
-	// population, and limit parametes
-
-	unordered_map<string, string> city_params {
-			{"min_pop","0"},
-			{"max_pop","200000"},
-			
-			{"limit", "666"}
-		};
-
-	vector<City>  us_cities = ds.getUSCities(city_params);
-	cout << "US Cities (tested for limit of 25 cities, population over 200K, and lat/long Bounding Box: (34.025348,-85.352783), (36.800488,-75.300293):\n";
-	for (auto c : us_cities)
-		cout << "\n" << c.getCity() << "," << c.getState() << ":" <<
-			" Population: " <<  c.getPopulation()  <<
-			", Elevation: "  <<  c.getElevation()
-			<< ", Lat/Long: " << c.getLatitude() << "," << c.getLongitude();
-
-    
-    GraphAdjList<string> city_graph;
-	vector<string> city_ids;
-
-	for (auto& c : us_cities) {
-    string id = c.getCity() + ", " + c.getState();
-    city_graph.addVertex(id);
-	city_ids.push_back(id);
-
-    // Optional: highlight big cities
-    // <-- Insert here
-	//setRandomNodeLocations(city_graph, city_ids, 2000);  // Spread nodes so they don't all stack in center
-}
-	// Ask user for two cities and highlight them if they exist
-string city1, city2;
-bool valid = false;
-while (!valid) {
-    cout << "\nEnter the first city: ";
-    getline(cin, city1);
-    cout << "Enter the second city: ";
-    getline(cin, city2);
-
-    string found1_id = "";
-    string found2_id = "";
-
-    // Search for the full vertex ID by city name
-    for (const auto& id : city_ids) {
-        if (id.find(city1) != string::npos) {
-            found1_id = id;
-        }
-        if (id.find(city2) != string::npos) {
-            found2_id = id;
-        }
-    }
-
-    if (!found1_id.empty() && !found2_id.empty()) {
-        city_graph.getVertex(found1_id)->getVisualizer()->setColor("red");
-        city_graph.getVertex(found2_id)->getVisualizer()->setColor("red");
-        valid = true;
-    } else {
-        cout << "One or both cities were not found in the dataset. Please try again.\n";
-    }
+vector<City> cities1 = ds.getUSCities(city_params1);
+if (cities1.empty()) {
+    cout << "No cities found in " << state1 << endl;
+    return 0;
 }
 
-	// setRandomNodeLocations(city_graph, city_ids,200);  // 2000 is the spread range, you can adjust as needed
+cout << "Cities found in " << state1 << ":" << endl;
+for (const auto& c : cities1) {
+    cout << c.getCity() << ", " << c.getState() << endl;
+}
 
-    
+// --- Input second state and get its cities ---
+string state2;
+cout << "\nEnter second state abbreviation (e.g., NC, CA, NY): ";
+getline(cin, state2);
 
-int neighbors = 3;  // number of neighbors per city
+unordered_map<string, string> city_params2 {
+    {"min_pop", "0"},
+    {"max_pop", "20000000"},
+    {"state", state2},
+    {"limit", "600"}
+};
 
-for (size_t i = 0; i < city_ids.size(); ++i) {
-    for (int j = 0; j < neighbors; ++j) {
-        int rand_idx = rand() % city_ids.size();
-        if (rand_idx != i) {
-            city_graph.addEdge(city_ids[i], city_ids[rand_idx]);
+vector<City> cities2 = ds.getUSCities(city_params2);
+if (cities2.empty()) {
+    cout << "No cities found in " << state2 << endl;
+    return 0;
+}
+
+cout << "Cities found in " << state2 << ":" << endl;
+for (const auto& c : cities2) {
+    cout << c.getCity() << ", " << c.getState() << endl;
+}
+
+// --- Ask user for one city from each state ---
+string city1_input, city2_input;
+string selectedCity1, selectedCity2;
+
+// Validate first city
+// Validate first city
+while (true) {
+    cout << "\nEnter the first city from " << state1 << ": ";
+    getline(cin, city1_input);
+
+    bool found = false;
+    for (const auto& c : cities1) {
+        if (c.getCity() == city1_input) {
+            selectedCity1 = c.getCity() + ", " + c.getState();
+            found = true;
+            break;
         }
     }
+    if (found) break;
+    cout << "City not found in " << state1 << ". Please try again.\n";
+}
+
+// Validate second city
+// Validate second city
+while (true) {
+    cout << "Enter the second city from " << state2 << ": ";
+    getline(cin, city2_input);
+
+    bool found = false;
+    for (const auto& c : cities2) {
+        if (c.getCity() == city2_input) {
+            selectedCity2 = c.getCity() + ", " + c.getState();
+            found = true;
+            break;
+        }
+    }
+    if (found) break;
+    cout << "City not found in " << state2 << ". Please try again.\n";
 }
 
 
+// --- Combine both states' cities into a single vector for the graph ---
+vector<City> all_cities = cities1;
+all_cities.insert(all_cities.end(), cities2.begin(), cities2.end());
+
+    GraphAdjList<string, double> city_graph;
+	//format example to access edge_weights["Raleigh, NC, Charlotte, NC"];
+	map<string, double> edge_weights;
+	
+	for(int i = 0; i < all_cities.size(); i++){
+    City c = all_cities[i];
+    string city_id = c.getCity() + ", " + c.getState();
+    city_graph.addVertex(city_id);
+}
+
+
+
+for(int i = 0; i < all_cities.size(); i++){
+    for(int j = i + 1; j < all_cities.size(); j++){
+        double distance = getDistance(all_cities[i].getLatitude(), all_cities[i].getLongitude(),
+                                      all_cities[j].getLatitude(), all_cities[j].getLongitude());
+        string city1 = all_cities[i].getCity() + ", " + all_cities[i].getState();
+        string city2 = all_cities[j].getCity() + ", " + all_cities[j].getState();
+        city_graph.addEdge(city1, city2, distance);
+        city_graph.addEdge(city2, city1, distance);
+
+         
+
+        edge_weights[city1 + "," + city2] = distance;
+        edge_weights[city2 + "," + city1] = distance;
+
+       
+    }
+}
+
+// --- Add labels for all cities ---
+for (int i = 0; i < all_cities.size(); i++) {
+    string city_id = all_cities[i].getCity() + ", " + all_cities[i].getState();
+    city_graph.getVertex(city_id)->setLabel(city_id); // show city label
+}
+
+
+
+
+
+
+
+	if(city_graph.getVertex(selectedCity1) == nullptr && city_graph.getVertex(selectedCity2) == nullptr){
+    cout << "Neither city found" << endl;
+}
+else if(city_graph.getVertex(selectedCity1) == nullptr){
+    cout << "First city not found" << endl;
+}
+else if(city_graph.getVertex(selectedCity2) == nullptr){
+    cout << "Second city not found" << endl;
+}
+else{
+    city_graph.getVertex(selectedCity1)->getVisualizer()->setColor("red");
+    city_graph.getVertex(selectedCity2)->getVisualizer()->setColor("red");
+    cout << "Showing fastest path from " << selectedCity1 << " to " << selectedCity2 << endl;
+}
 
 	
     bridges.setDataStructure(&city_graph);
@@ -148,3 +197,44 @@ for (size_t i = 0; i < city_ids.size(); ++i) {
 
 	return 0;
 }
+
+// #include "Bridges.h"
+// #include "DataSource.h"
+// #include "GraphAdjList.h"
+
+// #include <iostream>
+// #include <string>
+// #include <unordered_map>
+// #include <fstream>
+// #include "data_src/OSMData.h"
+// #include "data_src/OSMVertex.h"
+// #include "data_src/OSMEdge.h"
+
+// using namespace std;
+// using namespace bridges;
+
+// int main(int argc, char **argv) {
+
+// 	// create Bridges object
+// 	Bridges bridges (2, "AriyaM", "211314957910");
+
+// 	DataSource ds(bridges);
+// //	OSMData osm_data = ds.getOSMData("Charlotte, North Carolina");
+// 	OSMData osm_data = ds.getOSMData(41.03133177632377, -98.02593749997456,
+// 						42.008577297430456, -97.94531249997696);
+
+// 	vector<OSMVertex> vertices = osm_data.getVertices();
+// 	vector<OSMEdge> edges = osm_data.getEdges();
+
+// 	double coords[2];
+// 	cout << "Number of Vertices [Charlotte]:" << vertices.size() << endl;
+// 	cout << "Number of Edges [Charlotte]:" << edges.size() << endl;
+// 	cout << "edge length of edge " << edges[0].getEdgeLength() << endl;
+// 	cout << "destination of edge " << 
+
+// 	// get cartesian coordinate  location of first vertex
+// 	osm_data.getVertices()[0].getCartesianCoords(coords);
+// 	cout << "Location of first vertex [Cartesian Coord]: " <<  coords[0] << ","
+// 		<< coords[1] << endl;
+// 	return 0;
+// }
